975ecc3fec60c38223bc9cf040a3d620b3cf691b
Dann Luciano <dannluciano@gmail.com>
2021-08-12 08:48:10 -0300

    5 - Crie um endpoint para visualizar uma receita específica

--- src/recipes/recipe_controller.js
+++ src/recipes/recipe_controller.js
@@ -21,8 +21,21 @@ module.exports = {
         }
     },
     listRecipes: async (req, res) => {
-        const recipes = await recipeService.getAllRecipes();
-        return res.status(200).json(recipes);
+        try {
+            const recipes = await recipeService.getAllRecipes();
+            return res.status(200).json(recipes);
+        } catch (error) {
+            return res.status(error.errorCode).json({ message: error.message });
+        } 
+    },
+    showRecipe: async (req, res) => {
+        try {
+            const recipeId = req.params.id;
+            const recipe = await recipeService.getRecipeById(recipeId);
+            return res.status(200).json(recipe);
+        } catch (error) {
+            return res.status(error.errorCode).json({ message: error.message });
+        }
     },
     
 };
