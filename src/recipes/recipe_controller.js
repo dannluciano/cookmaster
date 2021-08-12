465a1004e1bfe7370398531143c25d26a39d015d
Dann Luciano <dannluciano@gmail.com>
2021-08-12 00:07:12 -0300

    Fix 3 - Crie um endpoint para o cadastro de receitas

--- src/recipes/recipe_controller.js
+++ src/recipes/recipe_controller.js
@@ -4,13 +4,16 @@ const { RecipeGenericException } = require('./recipe_exeptions');
 module.exports = {
     createRecipe: async (req, res) => {
         const { name, ingredients, preparation } = req.body;
+        const userId = req.currentUser.id;
 
         try {
             if (!name || !ingredients || !preparation) {
                 throw new RecipeGenericException();
             }
 
-            const recipe = await recipeService.createRecipe({ name, ingredients, preparation });
+            const recipe = await recipeService.createRecipe(
+                { name, ingredients, preparation, userId },
+            );
 
             return res.status(201).json({ recipe });
         } catch (error) {
